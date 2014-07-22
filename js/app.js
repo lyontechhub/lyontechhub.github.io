794d28be7a3e5141d5e15eff1213cef089c6f9ef
Clément Bouillier <clement.bouillier@devcrafting.com>
2014-07-22 12:20:54 +0200

    Angular.js routing with 4 static views : about, calendar, communities, welcome

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-var app = angular.module("lyontechhub", ["mgcrea.ngStrap.navbar"]);
+var app = angular.module("lyontechhub", ["mgcrea.ngStrap.navbar", "ngRoute"]);
+
+app.config(["$routeProvider", function($routeProvider) {
+	$routeProvider
+		.when("/", {
+			templateUrl: "views/welcome.html"
+		})
+		.when("/calendrier", {
+			templateUrl: "views/calendar.html"
+		})
+		.when("/communautes", {
+			templateUrl: "views/communities.html"
+		})
+		.when("/a-propos", {
+			templateUrl: "views/about.html"
+		})
+		.otherwise({
+			redirectTo: "/"
+		});
+}]);
