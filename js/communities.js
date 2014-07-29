161213fb66b2349e770951f2063b9ceeb9d33b03
Clement Bouillier <clement.bouillier@devcrafting.com>
2014-07-30 00:03:06 +0200

    Add community page with MUGLyon data example : global data + social links

--- js/communities.js
+++ js/communities.js
@@ -6,6 +6,32 @@ app.controller("CommunitiesViewModel", ["$http", function($http){
 
     $http.get("/data/communities.json").success(function(data) {
         self.communities = data;
-        $('body').scrollspy({ target: '.communitiesNav', offset: 50 })
+        // Does not work...$('body').scrollspy({ target: '.communitiesNav', offset: 50 })
     });
+}]);
+
+app.controller("CommunityViewModel", ["$routeParams", "$http", function($routeParams, $http) {
+    var self = this;
+    self.key = $routeParams.key;
+    self.community = {};
+
+    $http.get("/data/" + self.key + ".json").success(function(data) {
+        self.community = data;
+    });
+
+    $http.get("/data//communities.json").success(function(data) {
+        var currentCommunity = $.grep(data, function(item) {
+            return item.key === self.key;
+        });
+        $.extend(self.community, currentCommunity[0]);
+    });
+}]);
+
+app.config(["$routeProvider", function($routeProvider) {
+    $routeProvider
+        .when("/communaute/:key", {
+            templateUrl: "views/community.html",
+            controller: "CommunityViewModel",
+            controllerAs: "communityViewModel"
+        });
 }]);
