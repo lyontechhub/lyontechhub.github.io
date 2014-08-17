8cbd0b4d957063ed3da19a9f02e791fe77a41b2d
Clement Bouillier <clement.bouillier@devcrafting.com>
2014-08-17 17:32:22 +0200

    Watch source files

--- README.md
+++ README.md
@@ -27,28 +27,30 @@ Developers
 This web site is done with plain HTML/CSS3/Javascript and AngularJS.
 
 The website is hosted through GitHub pages at lyontechhub.github.io as Organization pages (cf. https://help.github.com/articles/user-organization-and-project-pages).
-Note that GitHub uses master branch to deply, that's why we have 'dev' branch as the default one, which contains source code, that is compiled through Gulp and pushed to master for deployment (see below).
+Note that GitHub uses master branch to deploy, that's why we have 'dev' branch as the default one, which contains source code, that is compiled through Gulp and pushed to master for deployment (see below).
 
 ### Source code
 
-*TODO* It is built upon Bootstrap, with LESS files for theme customisation.
+It is built upon Bootstrap, with LESS files for theme customisation (see Dev & build environment).
 
 AngularJS is used to organize code in views sharing a common layout and to adapt content according request result on JSON files stores in data directory.
 
 Index.html file is the main entry point that define AngularJS application. Each view is then stored in views directory.
 
-### JSON data
+### JSON data & images
 
-*TODO* The following JSON files are used to provide content to dynamic views :
+The following JSON files are used to provide content to dynamic views in 'dist/data' directory:
 
 * communities.json
 * communities/[one per community].json
 
+Images like community icons are in 'dist/imgs' directory.
+
+'dist/imgs' & 'dist/data' directories MUST be modified on 'master' branch ONLY to be put online.
+
 ### Dev & build environment
 
 Packages are managed through NPM and Bower. So you need NodeJS but just for NPM.
-
-*TODO* Gulp is used to build assets (CSS & JS) used by the site.
 
 After cloning, just type the following commands to have a  :
 
@@ -59,3 +61,11 @@ npm start
 ```
 
 Nb : npm start is configured to start a local Node web server
+
+Gulp is used to build assets (CSS & JS) used by the site. JSON & images are to be modified directly on master branch.
+
+* 'build' task is used to build JS, CSS (from LESS) and HTML to 'dist' directory ('npm start' launch 'build' task before running the local Node web server)
+* 'watch' task is used to watch changes on source JS, LESS & HTML files to trigger a build dynamically (never ending task)
+* 'deploy' task is used to commit 'dist' directory content to 'master' branch (excluding 'data' & 'imgs' directories which are modified on 'master' branch directly), specify '--push' to effectively deploy on GitHub pages (i.e push on GitHub master branch)
+
+So, to be well prepared for dev, launch 2 terminals, first one with 'npm start', and second one with 'node_modules\.bin\gulp watch'.
--- gulpfile.js
+++ gulpfile.js
@@ -4,38 +4,38 @@ var deploy = require("gulp-gh-pages");
 var less = require("gulp-less");
 var amdOptimize = require('amd-optimize');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var argv = require('yargs').argv;
+var gulpIf = require('gulp-if');
+var minifyCSS = require('gulp-minify-css');
 
 /* Configuration */
 var lessSource = 'css/main.less';
 var jsSource = 'js/**/*.js';
-var htmlSource = '**/*.html';
-var data = 'data/**/*';
-var images = 'imgs/**/*';
+var htmlSource = ['views/*.html', 'index.html'];
 var destDir = "dist/";
 
 /* Tasks */
 gulp.task('clean', function () {
-   del('dist', true);
-});
-
-gulp.task('copy-data', function () {
-   return gulp.src(data)
-       .pipe(gulp.dest(destDir + 'data'));
-});
-
-gulp.task('copy-images', function () {
-    return gulp.src(images)
-        .pipe(gulp.dest(destDir + 'imgs'));
+    del('dist/css', true);
+    del('dist/js', true);
+    del('dist/views', true);
+    del('dist/index.html', true);
 });
 
 gulp.task('build-css', function () {
-    return gulp.src(lessSource)
+    return gulp.src(lessSource, { base: './' })
         .pipe(less({ paths: [ 'bower_components/bootstrap/less/' ] }))
-        .pipe(gulp.dest(destDir + 'css'));
+        .pipe(gulpIf(argv.push, minifyCSS()))
+        .pipe(gulp.dest(destDir));
+});
+
+gulp.task('watch-css', function() {
+    gulp.watch(lessSource, ['build-css']);
 });
 
 gulp.task('build-js', function () {
-    return gulp.src(jsSource)
+    return gulp.src(jsSource, { base: './' })
         .pipe(amdOptimize('main', {
             baseUrl: 'js',
 
@@ -59,22 +59,34 @@ gulp.task('build-js', function () {
                     deps: ['angular']
                 }
             }}))
-        .pipe(concat('build.min.js'))
-        .pipe(gulp.dest(destDir + 'js'));
-});
-
-gulp.task('build-html', function () {
-    return gulp.src(htmlSource)
+        .pipe(concat('js/build.min.js'))
+        .pipe(gulpIf(argv.push, uglify()))
         .pipe(gulp.dest(destDir));
 });
 
-gulp.task('dev', ['copy-images', 'copy-data', 'build-css', 'build-js', 'build-html']);
+gulp.task('watch-js', function() {
+    gulp.watch(jsSource, ['build-js']);
+});
 
-gulp.task('deploy', function () {
+gulp.task('build-html', function () {
+    return gulp.src(htmlSource, { base: './' })
+        .pipe(gulp.dest(destDir));
+});
+
+gulp.task('watch-html', function() {
+    gulp.watch(jsSource, ['build-html']);
+});
+
+gulp.task('build', ['build-css', 'build-js', 'build-html']);
+
+gulp.task('watch', ['watch-css', 'watch-js', 'watch-html']);
+
+gulp.task('deploy', ['build'], function () {
+    var push = argv.push ? true : false;
     var options = {
         "branch": "master",
-        "push": false
+        "push": push
     }
-    gulp.src("./dist/**/*")
+    return gulp.src(['./dist/**/*', '!./dist/data/**/*', '!./dist/imgs/**/*'])
         .pipe(deploy(options));
 });
