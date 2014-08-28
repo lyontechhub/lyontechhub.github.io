fdc0ba2763bb4954dc5dff5061e84a5b81acdc15
Clement Bouillier <clement.bouillier@devcrafting.com>
2014-08-28 18:50:27 +0200

    Test to solve start failure on Heroku

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ app.use(require('prerender-node')
         console.log('bot request url : ' + req.url);
         done();
     }));
-app.use('/css', express.static(__dirname + '/css'));
+/*app.use('/css', express.static(__dirname + '/css'));
 app.use('/js', express.static(__dirname + '/js'));
 app.use('/views', express.static(__dirname + '/views'));
 app.use('/data', express.static(__dirname + '/data'));
-app.use('/imgs', express.static(__dirname + '/imgs'));
+app.use('/imgs', express.static(__dirname + '/imgs'));**/
 app.use('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
