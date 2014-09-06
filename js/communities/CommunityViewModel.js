fcc99122bd2ce9f1633565a4c94d1e5cf94f4ab0
Clement Bouillier <clement.bouillier@devcrafting.com>
2014-09-06 10:18:37 +0200

    Add past and next events in community page

--- js/communities/CommunityViewModel.js
+++ js/communities/CommunityViewModel.js
@@ -1,13 +1,20 @@
-define(['./module', './communitiesRepository', 'jquery'], function(app) {
+define(['./module', './communitiesRepository', './eventsRepository', 'jquery'], function(app) {
     'use strict';
 
-    app.controller("CommunityViewModel", ["$routeParams", "communitiesRepository", function($routeParams, communitiesRepository) {
+    app.controller("CommunityViewModel", ["$routeParams", "communitiesRepository", "eventsRepository", function($routeParams, communitiesRepository, eventsRepository) {
         var self = this;
         self.key = $routeParams.key;
         self.community = {};
+        self.community.pastEvents = [ {
+            title: "test",
+            startDate: new Date('2014-04-01 19:00'),
+            endDate: new Date('2014-04-01 22:00'),
+            location: 'test',
+            description: 'DEsqgs dgh fh sdffhs  fhfshsdfg qfh sfdh qfdhgsdfh sdfhsdfh sdfhsdfh sdfh sdfh sdfh sdfhs dsdfhsdfh sdfhsdfh .' }];
 
         communitiesRepository.getOne(self.key).success(function(data) {
-            self.community = data;
+            $.extend(self.community, data);
+            getEvents(self.community.patternsGoogleCalendar);
         });
 
         communitiesRepository.getAll().success(function(data) {
@@ -16,5 +23,17 @@ define(['./module', './communitiesRepository', 'jquery'], function(app) {
             });
             $.extend(self.community, currentCommunity[0]);
         });
+
+        var getEvents = function(pattern) {
+            var now = new Date();
+            var fourMonthAgo = new Date();
+            fourMonthAgo.setDate(fourMonthAgo.getDate() - 4 * 30);
+            var fourMonthLater = new Date();
+            fourMonthLater.setDate(fourMonthLater.getDate() + 4 * 30);
+            eventsRepository.getCommunityEvents(pattern, fourMonthAgo, fourMonthLater).then(function(events) {
+                self.community.pastEvents = $.grep(events, function(item) { return item.startDate < now; });
+                self.community.nextEvents = $.grep(events, function(item) { return item.startDate > now; });
+            });
+        }
     }]);
 });
---
+++ js/communities/eventsRepository.js
@@ -0,0 +1,31 @@
+define(['./module'], function(app) {
+    'use strict';
+
+    var mapEvents = function(googleEvent) {
+        return {
+            title: googleEvent.summary,
+            description: googleEvent.description,
+            location: googleEvent.location,
+            startDate: Date.parse(googleEvent.start.dateTime),
+            endDate: Date.parse(googleEvent.end.dateTime)
+        }
+    };
+
+    var eventsRepository = function($http, $q) {
+        this.getCommunityEvents = function(patterns, minDate, maxDate) {
+            var deferred = $q.defer();
+            $http.get("https://www.googleapis.com/calendar/v3/calendars/ck2ruq6cqfch3t4gshbd6vdnd4@group.calendar.google.com/events?key=AIzaSyAHKI9T7fhK68b2eggUCHlu9eOwsdFUrhg&singleEvents=true"
+                        + "&q=" + patterns[0] + "&orderBy=startTime"
+                        + "&timeMin=" + minDate.toISOString()
+                        + "&timeMax=" + maxDate.toISOString())
+                .success(function(data) {
+                    deferred.resolve(data.items.map(mapEvents));
+                });
+            return deferred.promise;
+        };
+    };
+
+    app.factory('eventsRepository', ['$http', '$q', function($http, $q) {
+        return new eventsRepository($http, $q);
+    }]);
+});
