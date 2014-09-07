467e1a4799bcef1d4077ccc1e6d600979bd473dd
Clement Bouillier <clement.bouillier@devcrafting.com>
2014-09-07 23:25:00 +0200

    Use several patterns to search events in Google Calendar (if any use community key as pattern)  + add LyonJug as pattern for this community

--- js/communities/CommunityViewModel.js
+++ js/communities/CommunityViewModel.js
@@ -6,34 +6,27 @@ define(['./module',
 
     app.controller("CommunityViewModel", ["$routeParams", "communitiesRepository", "eventsRepository", function($routeParams, communitiesRepository, eventsRepository) {
         var self = this;
-        self.key = $routeParams.key;
-        self.community = {};
-        self.community.pastEvents = [ {
-            title: "test",
-            startDate: new Date('2014-04-01 19:00'),
-            endDate: new Date('2014-04-01 22:00'),
-            location: 'test',
-            description: 'DEsqgs dgh fh sdffhs  fhfshsdfg qfh sfdh qfdhgsdfh sdfhsdfh sdfhsdfh sdfh sdfh sdfh sdfhs dsdfhsdfh sdfhsdfh .' }];
+        self.community = { key: $routeParams.key };
 
-        communitiesRepository.getOne(self.key).success(function(data) {
+        communitiesRepository.getOne(self.community.key).success(function(data) {
             $.extend(self.community, data);
-            getEvents(self.community.patternsGoogleCalendar);
+            getEvents(self.community);
         });
 
         communitiesRepository.getAll().success(function(data) {
             var currentCommunity = $.grep(data, function(item) {
-                return item.key === self.key;
+                return item.key === self.community.key;
             });
             $.extend(self.community, currentCommunity[0]);
         });
 
-        var getEvents = function(pattern) {
+        var getEvents = function(community) {
             var now = new Date();
             var fourMonthAgo = new Date();
             fourMonthAgo.setDate(fourMonthAgo.getDate() - 4 * 30);
             var fourMonthLater = new Date();
             fourMonthLater.setDate(fourMonthLater.getDate() + 4 * 30);
-            eventsRepository.getCommunityEvents(pattern, fourMonthAgo, fourMonthLater).then(function(events) {
+            eventsRepository.getCommunityEvents(community, fourMonthAgo, fourMonthLater).then(function(events) {
                 self.community.pastEvents = $.grep(events, function(item) { return item.startDate < now; });
                 self.community.nextEvents = $.grep(events, function(item) { return item.startDate > now; });
             });
--- js/communities/eventsRepository.js
+++ js/communities/eventsRepository.js
@@ -13,15 +13,28 @@ define(['./module'], function(app) {
     };
 
     var eventsRepository = function($http, $q) {
-        this.getCommunityEvents = function(patterns, minDate, maxDate) {
+        this.getCommunityEvents = function(community, minDate, maxDate) {
+            var patterns = community.patternsGoogleCalendar;
+            if (patterns == undefined){
+                patterns = [ community.key ];
+            }
             var deferred = $q.defer();
-            $http.get("https://www.googleapis.com/calendar/v3/calendars/ck2ruq6cqfch3t4gshbd6vdnd4@group.calendar.google.com/events?key=AIzaSyAHKI9T7fhK68b2eggUCHlu9eOwsdFUrhg&singleEvents=true"
-                        + "&q=" + patterns[0] + "&orderBy=startTime"
-                        + "&timeMin=" + minDate.toISOString()
-                        + "&timeMax=" + maxDate.toISOString())
-                .success(function(data) {
-                    deferred.resolve(data.items.map(mapEvents));
-                });
+            var asyncGoogleApiCalls = [];
+            for(var index = 0; index < patterns.length; index++) {
+                asyncGoogleApiCalls.push($http.get("https://www.googleapis.com/calendar/v3/calendars/ck2ruq6cqfch3t4gshbd6vdnd4@group.calendar.google.com/events?key=AIzaSyAHKI9T7fhK68b2eggUCHlu9eOwsdFUrhg&singleEvents=true"
+                    + "&q=" + patterns[index]
+                    + "&timeMin=" + minDate.toISOString()
+                    + "&timeMax=" + maxDate.toISOString()));
+            }
+
+            $q.all(asyncGoogleApiCalls).then(function(result){
+                var events = [];
+                for(var index = 0; index < result.length; index++) {
+                    events = events.concat(result[index].data.items.map(mapEvents));
+                }
+                deferred.resolve(events);
+            });
+
             return deferred.promise;
         };
     };
