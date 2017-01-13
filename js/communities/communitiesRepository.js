a9fc37876e14a67fd9a7eb7324839af87c2580dc
Florent FAYOLLE <florent.fayolle69@gmail.com>
2017-01-13 23:00:15 +0100

    Support other image format

--- js/communities/communitiesRepository.js
+++ js/communities/communitiesRepository.js
@@ -1,12 +1,20 @@
 define(['./module'], function(app) {
     'use strict';
 
+    var addImages = function(community) {
+        if (!community.image) {
+            community.image = community.key + ".png";
+        }
+    };
+
     var communitiesRepository = function($http) {
         this.getOne = function(key) {
-            return $http.get("/data/" + key + ".json");
+            return $http.get("/data/" + key + ".json").success(addImages);
         };
         this.getAll = function() {
-            return $http.get("/data/communities.json");
+            return $http.get("/data/communities.json").success(function(communities) {
+                return communities.forEach(addImages);
+            });
         };
     }
 
