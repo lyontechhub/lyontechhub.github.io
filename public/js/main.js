ce05cdf3d8970d60981f33536cfc61649a60bc38
Anthony Rey <gnuk@mailoo.org>
2024-12-14 12:48:47 +0100

    Use new calendar for upcoming events

--- public/js/main.js
+++ public/js/main.js
@@ -15,56 +15,60 @@ function dynamicFilter(inputId) {
     }
 }
 
-function fetchEvents(patterns, minDate, maxDate) {
-    return Promise.all(patterns.map((p) =>
-        fetch("https://www.googleapis.com/calendar/v3/calendars/ck2ruq6cqfch3t4gshbd6vdnd4@group.calendar.google.com/events?key=AIzaSyAHKI9T7fhK68b2eggUCHlu9eOwsdFUrhg&singleEvents=true"
-            + "&q=" + p
-            + "&timeMin=" + minDate.toISOString()
-            + "&timeMax=" + maxDate.toISOString())
-            .then((response) => response.json())))
-        .then((results) => {
-            var mapEvents = (googleEvent) => {
-                var startDate = Date.parse(googleEvent.start.dateTime);
-                var endDate = Date.parse(googleEvent.end.dateTime);
-                var parsedStartDate = new Date(startDate);
-                var parsedEndDate = new Date(endDate);
-                var format = (d) => d.toString().padStart(2, '0');
-                var formatHour = (d) => format(d.getHours()) + 'H' + format(d.getMinutes())
-                var months = ['Jan', 'Fev', 'Mars', 'Avr', 'Mai', 'Juin', 'Juil', 'Aout', 'Sept', 'Oct', 'Nov', 'Dec'];
-                return {
-                    id: googleEvent.id,
-                    title: googleEvent.summary,
-                    description: googleEvent.description,
-                    hasDescription: googleEvent.description && googleEvent.description.length > 0,
-                    location: googleEvent.location,
-                    startDate: startDate,
-                    endDate: endDate,
-                    url: googleEvent.htmlLink,
-                    startDateMonth: months[parsedStartDate.getMonth()],
-                    startDateDay: format(parsedStartDate.getDay()),
-                    startDateHour: formatHour(parsedStartDate),
-                    endDateHour: formatHour(parsedEndDate)
-                }
-            };
+const toDescription = component => {
+    const description = component.getFirstPropertyValue('description');
+    if (description === null) {
+        return '';
+    }
+    return description;
+};
 
-            var removeDuplicates = (events) => {
-                var tmpEvents = {};
-                for (var i = 0, n = events.length; i < n; i++) {
-                    tmpEvents[events[i].id] = events[i];
-                }
-                var i = 0;
-                events = [];
-                for (var id in tmpEvents) {
-                    events[i++] = tmpEvents[id];
-                }
-                return events;
-            };
+const meetupUrlFor = description => {
+    const matching = description.match(/(https:\/\/www.meetup.com\/[a-zA-Z0-9-]+\/events\/[0-9]+)/g);
+    if (matching && matching.length > 0) {
+        return matching[matching.length - 1];
+    }
+    return undefined;
+};
 
-            return Promise.resolve(removeDuplicates(results
-                .reduce((events, result) => events.concat(result.items.map(mapEvents)), [])
-            ));
-        });
+const toEvent = (component) => {
+    const description = toDescription(component);
+    const startDate = component.getFirstPropertyValue('dtstart').toJSDate();
+    const endDate = component.getFirstPropertyValue('dtend').toJSDate();
+    const format = (d) => d.toString().padStart(2, '0');
+    const formatHour = (d) => format(d.getHours()) + 'H' + format(d.getMinutes());
+    const months = ['Jan', 'Fev', 'Mars', 'Avr', 'Mai', 'Juin', 'Juil', 'Aout', 'Sept', 'Oct', 'Nov', 'Dec'];
+    const url = meetupUrlFor(description);
+    return {
+        id: component.getFirstPropertyValue('uid'),
+        title: component.getFirstPropertyValue('summary'),
+        description: description,
+        hasDescription: description && description.length > 0,
+        url,
+        hasUrl: url !== undefined,
+        startDate,
+        endDate,
+        location: component.getFirstPropertyValue('location'),
+        startDateMonth: months[startDate.getMonth()],
+        startDateDay: format(startDate.getDate()),
+        startDateHour: formatHour(startDate),
+        endDateHour: formatHour(endDate)
+    }
 }
+
+const matchPatternForEvent = event => pattern => event.title.toLowerCase().includes(`[${pattern.toLowerCase()}]`);
+
+const matchForPatterns = patterns => event => patterns.some(matchPatternForEvent(event));
+
+const filterForPeriod = (minDate, maxDate) => event => event.startDate >= minDate && event.endDate <= maxDate;
+
+const listVEventComponents = raw => new ICAL.Component(ICAL.parse(raw)).getAllSubcomponents('vevent');
+
+const fetchEvents = (patterns, minDate, maxDate) => fetch('https://www.lyontechhub.org/Lyon-Tech-Hub-Calendar/calendar.ics').then((response) => response.text()).then((raw) =>
+    listVEventComponents(raw)
+        .map(toEvent)
+        .filter(filterForPeriod(minDate, maxDate))
+        .filter(matchForPatterns(patterns)));
 
 function displayEvents(template, element, events) {
     if (element) {
