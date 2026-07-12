43f936ffaee549529607c708f45b85bda083814a
fpellet <git@florentpellet.com>
2026-07-12 19:26:38 +0200

    Remove dead code

--- src/lib/communities.test.ts
+++ src/lib/communities.test.ts
@@ -1,4 +1,4 @@
-import { expect, test, describe, beforeEach } from 'vitest'
+import { expect, test, describe } from 'vitest'
 import { extractCalendars, getSocialDisplayData, type Community } from './communities'
 
 describe('communities', () => {
